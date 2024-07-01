895fa8b5350d773b28ade40f791e95a4b9892f5e
Marius Apostu <apostu_marius_lucian@yahoo.co.uk>
2024-07-01 08:00:03 +0300

    page factory 1st commit

---
+++ src/main/java/pageObjects/MyAccountPage.java
@@ -0,0 +1,33 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import selenium.utils.SeleniumWrappers;
+
+public class MyAccountPage extends SeleniumWrappers{
+
+	public MyAccountPage(WebDriver driver) {
+		super(driver);
+		PageFactory.initElements(driver, this);
+	}
+
+	
+	@FindBy(id="username")
+	public WebElement userfield;
+	
+	@FindBy(id="password")
+	public WebElement passField;
+	
+	@FindBy(name="login")
+	public WebElement submitButton;
+	
+	public void loginInApp(String user, String pass) {
+		
+		sendKeys(userfield, user);
+		sendKeys(passField, pass);
+		click(submitButton);
+	}
+}
